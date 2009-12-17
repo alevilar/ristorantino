6efdf68ea2711306f41bb2ce669c994f67efad38
alevilar <alevilar@841e6886-8446-11de-8c29-95e9a393758c>
2009-12-17 19:52:01 +0000

    nueva version del demonio del spooler se solucionaron varios problemas, como por ejemplo el restart. ademas el stop se mejoro cambiando alguns de los parametros del KILL -9

--- app/vendors/spooler/hasar_spooler.sh
+++ app/vendors/spooler/hasar_spooler.sh
@@ -9,7 +9,6 @@ while true ; do
 		
 	for b in $DBS ;
 	do
-		#echo $b
 		#cd /var/www/ristorantino/app/vendors/spooler
 	       	spooler -p$b -s /tmp/fuente -a /tmp/dest -l -d /tmp/spooler.log -b /tmp
 	done
--- app/vendors/spooler/hasar_spooler_keep_alive.sh
+++ app/vendors/spooler/hasar_spooler_keep_alive.sh
@@ -1,25 +1,29 @@
-set -x
+#set -x
 #!/bin/bash
 
 CARPETA_FUENTE=/tmp/fuente
 
-while true ; do	
-	cd $CARPETA_FUENTE
-	CANT=`ls -1 | wc -l`			
-	
-	#si es mayor cero quiere decir que hay archios en la carpeta para imprimir
-	if [$CANT -eq 0]
-	then
-		cd /dev
-		DBS=`ls ttyUSB*`		
-		
-		for b in $DBS ;
-		do
-			echo $b
-			spooler -p$b -l -c "*" 
-		done
-	fi		
-	sleep 220
+while true ; do
+	if [ -d "$CARPETA_FUENTE" ]; then
+		cd $CARPETA_FUENTE
+		CANT=`ls -1 | wc -l`	
+
+		#si es mayor cero quiere decir que hay archios en la carpeta para imprimir
+		if [ $CANT -eq 0 ]
+		then
+			cd /dev
+			DBS=`ls ttyUSB*`		
+			
+			for b in $DBS ;
+			do
+				echo "Conectado en dispositivo $b"
+				spooler -p$b -l -c "*" 
+				echo "se envio el comando"
+			done
+		fi		
+	fi
+	sleep 200
+
 	RETVAL=$?
 done
 exit $RETVAL
