66feaa1a2d2585c8d72d0c1778dba3b66842c482
Ale Vilar <alevilar@gmail.com>
2014-11-06 01:09:57 -0300

    core con el debug ahora en risto.php

--- Config/core.php
+++ Config/core.php
@@ -19,26 +19,15 @@
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
+
+
+	
 if (file_exists(APP . 'Config' . DS . 'risto.php')) {
 	require APP . 'Config' . DS . 'risto.php';
 }
 
 setlocale(LC_TIME ,"es_AR.utf8");
 
-/**
- * CakePHP Debug Level:
- *
- * Production Mode:
- * 	0: No error messages, errors, or warnings shown. Flash messages redirect.
- *
- * Development Mode:
- * 	1: Errors and warnings shown, model caches refreshed, flash messages halted.
- * 	2: As in 1, but also with full debug messages and SQL output.
- *
- * In production mode, flash messages redirect after a time interval.
- * In development mode, you need to click the flash message to continue.
- */
-	Configure::write('debug', 2);
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
